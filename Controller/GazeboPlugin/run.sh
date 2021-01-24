9682d849e5a58a90aef3a03d163a1fdb438fb703
lstar93 <starcu93@gmail.com>
2021-01-24 15:20:44 +0100

    run script added

---
+++ Controller/GazeboPlugin/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+arg=""
+
+if [ "$1" = "verbose" ]; then
+    arg="--verbose"
+fi
+
+if [ -f "libroboarm_plugin.so" ]; then
+    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/lukas/Desktop/RoboArmGIT/RoboArmAI/3DModel/
+    export GAZEBO_IP=127.0.0.1
+    export LD_LIBRARY_PATH=${PWD}
+    gazebo $arg roboarm.world
+else
+    echo "Can't fing plugin library, please run make.sh to build first!"
+fi
