732dead3c6eb1acf64f6f847e8499ef90b3c174a
lstar93 <starcu93@gmail.com>
2021-01-25 16:01:47 +0100

    header file added to gazebo plugin

--- Controller/GazeboPlugin/roboarm_plugin.cpp
+++ Controller/GazeboPlugin/roboarm_plugin.cpp
@@ -1,46 +1,33 @@
 #ifndef _VELODYNE_PLUGIN_HH_
 #define _VELODYNE_PLUGIN_HH_
 
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
+#include "roboarm_plugin.h"
 
 namespace gazebo
 {
-  /// \brief A plugin to control a Velodyne sensor.
-  class RoboArmPlugin : public ModelPlugin
+  void RoboArmPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   {
-    /// \brief Constructor
-  public:
-    RoboArmPlugin() {}
-
-    /// \brief The load function is called by Gazebo when the plugin is
-    /// inserted into simulation
-    /// \param[in] _model A pointer to the model that this plugin is
-    /// attached to.
-    /// \param[in] _sdf A pointer to the plugin's SDF element.
-    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    // Safety check
+    if (_model->GetJointCount() == 0)
     {
-      // Safety check
-      if (_model->GetJointCount() == 0)
-      {
-        std::cerr << "Invalid joint count, Velodyne plugin not loaded\n";
-        return;
-      }
+      std::cerr << "Invalid joint count, Velodyne plugin not loaded\n";
+      return;
+    }
 
-      // Store the model pointer for convenience.
-      this->model = _model;
+    // Store the model pointer for convenience.
+    this->model = _model;
 
-      // Get the first joint. We are making an assumption about the model
-      // having one joint that is the rotational joint.
-      this->position_pid = common::PID(20, 1, 20); // Setup a PID-controller.
-      for (const auto &j_num : modelJointIDs)
-      {
-        auto joint = _model->GetJoints()[j_num];
-        setJointPositionPID(joint, position_pid);
-        SetJointTargetPosition(joint, 0.75);
-      }
+    // Get the first joint. We are making an assumption about the model
+    // having one joint that is the rotational joint.
+    this->position_pid = common::PID(20, 1, 20); // Setup a PID-controller.
+    for (const auto &j_num : modelJointIDs)
+    {
+      auto joint = _model->GetJoints()[j_num];
+      setJointPositionPID(joint, position_pid);
+      SetJointTargetPosition(joint, 0.75);
+    }
 
-      /*
+    /*
       // set first joint
       joint_0 = _model->GetJoints()[0];
       
@@ -56,44 +43,28 @@ namespace gazebo
       this->model->GetJointController()->SetPositionTarget(
           this->joint_0->GetScopedName(), 0.75);
       */
-    }
+  }
 
-    void setJointPositionPID(physics::JointPtr p_joint, common::PID &r_pid)
+  void RoboArmPlugin::setJointPositionPID(physics::JointPtr p_joint, common::PID &r_pid)
+  {
+    this->model->GetJointController()->SetPositionPID(
+        p_joint->GetScopedName(), r_pid);
+  }
+
+  void RoboArmPlugin::SetJointTargetPosition(physics::JointPtr p_joint, double pos_in_radians)
+  {
+    if (pos_in_radians > 1.57)
     {
-      this->model->GetJointController()->SetPositionPID(
-          p_joint->GetScopedName(), r_pid);
+      std::cout << "ERROR: target postion can't be larger than 1.57" << std::endl;
+      pos_in_radians = 1.57;
     }
-
-    void SetJointTargetPosition(physics::JointPtr p_joint, double pos_in_radians)
+    else if (pos_in_radians < -1.57)
     {
-      if (pos_in_radians > 1.57)
-      {
-        std::cout << "ERROR: target postion can't be larger than 1.57" << std::endl;
-        pos_in_radians = 1.57;
-      }
-      else if (pos_in_radians < -1.57)
-      {
-        std::cout << "ERROR: target postion can't be less than -1.57" << std::endl;
-        pos_in_radians = -1.57;
-      }
-      this->model->GetJointController()->SetPositionTarget(
-          p_joint->GetScopedName(), pos_in_radians);
+      std::cout << "ERROR: target postion can't be less than -1.57" << std::endl;
+      pos_in_radians = -1.57;
     }
-
-  private:
-    /// \brief Pointer to the model.
-    physics::ModelPtr model;
-
-    /// \brief Pointer to the joint.
-    // physics::JointPtr joint_0;
-
-    const std::vector<uint8_t> modelJointIDs{0, 2, 6, 9, 12};
-
-    /// \brief A PID controller for the joint.
-    common::PID position_pid;
-  };
-
-  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
-  GZ_REGISTER_MODEL_PLUGIN(RoboArmPlugin)
+    this->model->GetJointController()->SetPositionTarget(
+        p_joint->GetScopedName(), pos_in_radians);
+  }
 } // namespace gazebo
 #endif
---
+++ Controller/GazeboPlugin/roboarm_plugin.h
@@ -0,0 +1,43 @@
+#ifndef _ROBOARM_PLUGIN_HH_
+#define _ROBOARM_PLUGIN_HH_
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+
+namespace gazebo
+{
+  /// \brief A plugin to control a ROBOARM sensor.
+  class RoboArmPlugin : public ModelPlugin
+  {
+    /// \brief Constructor
+  public:
+    RoboArmPlugin() {}
+
+    /// \brief The load function is called by Gazebo when the plugin is
+    /// inserted into simulation
+    /// \param[in] _model A pointer to the model that this plugin is
+    /// attached to.
+    /// \param[in] _sdf A pointer to the plugin's SDF element.
+    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+    void setJointPositionPID(physics::JointPtr p_joint, common::PID &r_pid);
+    
+    void SetJointTargetPosition(physics::JointPtr p_joint, double pos_in_radians);
+
+  private:
+    /// \brief Pointer to the model.
+    physics::ModelPtr model;
+
+    /// \brief Pointer to the joint.
+    // physics::JointPtr joint_0;
+
+    const std::vector<uint8_t> modelJointIDs{0, 2, 6, 9, 12};
+
+    /// \brief A PID controller for the joint.
+    common::PID position_pid;
+  };
+
+  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
+  GZ_REGISTER_MODEL_PLUGIN(RoboArmPlugin)
+} // namespace gazebo
+#endif
