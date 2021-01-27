0d7cc85f41bceed2bec59b7ba9e226b7b8a30e40
lstar93 <starcu93@gmail.com>
2021-01-27 18:20:52 +0100

    arm configuration subscriber done

--- Controller/ROS/src/robo_arm_controller/src/joint_controller.cpp
+++ Controller/ROS/src/robo_arm_controller/src/joint_controller.cpp
@@ -1,8 +1,31 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float64MultiArray.h"
-
 #include <vector>
+#include <thread>
+#include <chrono>
+#include "json/json.h"
+
+Json::Value configuration;
+bool configurationReady = false;
+void roboArmConfigurationCallback(const std_msgs::String::ConstPtr &inStr)
+{
+    std::string strJson = inStr->data.c_str();
+
+    ROS_INFO("strJson: %s", strJson.c_str());
+
+    const auto strJsonLength = static_cast<int>(strJson.length());
+    JSONCPP_STRING err;
+
+    Json::CharReaderBuilder builder;
+    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    if (!reader->parse(strJson.c_str(), strJson.c_str() + strJsonLength, &configuration, &err))
+    {
+        ROS_INFO("roboArmConfigurationCallback ERROR");
+        return;
+    }
+    configurationReady = true;
+}
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -23,8 +46,31 @@ int main(int argc, char **argv)
    */
     int count = 0;
     // Create two sets of arm possitions
-    std::vector<double> pose_0{1.0, 1.0, 1.0, 1.0, 1.0};
-    std::vector<double> pose_1{0.5, 0.5, 0.5, 0.5, 0.5};
+    std::vector<double> pose_0;
+    std::vector<double> pose_1;
+
+    // Create robot configuration subscriber
+    // example configuration JSON: {"joints_configuration":{"number_of_joints":5},"name":"RoboArm"}
+    auto roboArmConfigurationSubscriber = n.subscribe("/my_robot/configuration", 1000, roboArmConfigurationCallback);
+
+    while (!configurationReady)
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    int numberOfJoints = configuration["joints_configuration"]["number_of_joints"].asInt();
+    ROS_INFO("numberOfJoints: %d", numberOfJoints);
+    for (int i = 0; i < numberOfJoints; ++i)
+    {
+        pose_0.push_back(1.0); // fill pose 0 with 1.0 poses
+        pose_1.push_back(0.5); // fill pose 0 with 0.5 poses
+    }
+
+    if (numberOfJoints == -1)
+    {
+        std::cout << "Ups, something went wrong with configuration subscriber, numberOfJoints is -1!" << std::endl;
+    }
 
     uint8_t toggler = 0;
     while (ros::ok())
