b5fdfbe0068283ad444ee5deace4b9329fec74ea
lstar93 <starcu93@gmail.com>
2021-02-17 19:01:51 +0100

    prints refactoring

--- Controller/ROS/src/controller/scripts/robot_kinematics.py
+++ Controller/ROS/src/controller/scripts/robot_kinematics.py
@@ -51,7 +51,7 @@ import matplotlib.pyplot as plt
 np.set_printoptions(suppress=True)
 
 # Keep some prints, but sho them only if necessary
-def PRINT_MSG(msg, VERBOSE=True):
+def PRINT_MSG(msg, VERBOSE=False):
     if(VERBOSE):
         print(msg)
 
@@ -89,11 +89,11 @@ def rotation_matrix(rot_joint, angle, size = 3):
 # Translation -> move axis by vector 
 # Transformation -> translation + rotation by angle
 # vect = position vector
-# rtm  = rotation matrix, 3x3 identity matrix if no angle given
 def translation_matrix(vect, axis='', angle=0):
     if len(vect) != 3:
         raise Exception('Incorrect vector size, vector dimension should be (1, 3) -> [x y z]')
     else:
+        # rtm  = rotation matrix, 3x3 identity matrix if no angle given
         rtm = np.identity(4) if not axis else rotation_matrix(axis, angle, 4)
         for x in range(3):
             rtm[x,3] = vect[x] # fit translated vector x into last column of rotatiom or identity matrix
@@ -137,13 +137,11 @@ class Point:
     x = 0.0
     y = 0.0
     z = 0.0
-    xyz = []
 
     def __init__(self, xyz):
-            self.x = xyz[0]
-            self.y = xyz[1]
-            self.z = xyz[2]
-            self.xyz = xyz
+        self.x = xyz[0]
+        self.y = xyz[1]
+        self.z = xyz[2]
 
     def distance_to_point(self, p):
         ret = sqrt(pow((self.x - p.x), 2) + pow((self.y - p.y), 2) + pow((self.z - p.z), 2))
@@ -164,13 +162,15 @@ class Point:
     def to_list(self):
         return [self.x, self.y, self.z]
 
-# matplotlib cannot resize all axis to same scale so e-12 numbers made plots impossible to analyze -> round very low numbers to 0
+# matplotlib cannot resize all axis to the same scale so very small numbers make plots impossible to analyze 
+# thus all very small numbers (assume e-10 and less until -e-10) will be rounded to 0 for plotting only
 def round(num):
-    if num > pow(e,-10) and num < 0:
+    if num > -1*e**-10 and num < 1*e**-10:
         return 0
     return num
 
 # FABRIK stands from forward and backward reaching inverse kinematics -> https://www.youtube.com/watch?v=UNoX65PRehA&feature=emb_title -> most commonly used this times
+# TODO: add angles limits
 class Fabrik:
 
     init_joints_positions = []
@@ -184,22 +184,20 @@ class Fabrik:
         self.err_margin = err_margin
         self.max_iter_num = max_iter_num
 
-    def plot(self, start_point, goal_point, joints_final_positions):
+    def plot_joints_and_points(self, joints, points = []):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-        ax.scatter(start_point.x, start_point.y, start_point.z, color='blue')
-        base_point = Point([0, 0, 0])
-        base = [base_point, joints_final_positions[0]]
-        ax.scatter([round(x.x) for x in base], [round(x.y) for x in base], [round(x.z) for x in base], color='green')
-        ax.plot3D([round(x.x) for x in base], [round(x.y) for x in base], [round(x.z) for x in base], color='green')
-        ax.scatter([round(x.x) for x in self.init_joints_positions], [round(x.y) for x in self.init_joints_positions], [round(x.z) for x in self.init_joints_positions], color='green')
-        ax.plot3D([round(x.x) for x in self.init_joints_positions], [round(x.y) for x in self.init_joints_positions], [round(x.z) for x in self.init_joints_positions], color='green')
-        ax.scatter([round(x.x) for x in joints_final_positions], [round(x.y) for x in joints_final_positions], [round(x.z) for x in joints_final_positions], color='pink')
-        ax.plot3D([round(x.x) for x in joints_final_positions], [round(x.y) for x in joints_final_positions], [round(x.z) for x in joints_final_positions], color='pink')
-        ax.scatter(round(goal_point.x), round(goal_point.y), round(goal_point.z), color='orange')
+        colors = [list(x) for x in numpy.random.rand(len(joints) + len(points),3)] 
+        for j,c in zip(joints[0:len(joints)],colors):
+            ax.scatter([round(x.x) for x in j], [round(x.y) for x in j], [round(x.z) for x in j], color=c)
+            ax.plot3D([round(x.x) for x in j], [round(x.y) for x in j], [round(x.z) for x in j], color=c)
+
+        for p,c in zip(joints[len(joints):],colors):
+            ax.scatter(round(p.x), round(p.y), round(p.z), color=c)
+
         plt.show()
         
     # Compute backward iteration
@@ -239,7 +237,7 @@ class Fabrik:
         current_join_positions = self.init_joints_positions
         goal_joints_positions = []
         start_point = self.init_joints_positions[0]
-        goal_point = Point([goal_eff_pos[0], goal_eff_pos[1], goal_eff_pos[2]])
+        goal_point = Point([x for x in goal_eff_pos])
         start_error = 1
         goal_error = 1
         iter_cnt = 0
@@ -255,13 +253,15 @@ class Fabrik:
             iter_cnt = iter_cnt + 1
 
         #if(verbose and not len(goal_joints_positions) == 0):
-        #    self.plot(start_point, goal_point, goal_joints_positions)
+        #    base_point = Point([0, 0, 0])
+        #    base = [base_point, goal_joints_positions[0]]
+        #    self.plot_joints_and_points([base, self.init_joints_positions, goal_joints_positions], [base_point, goal_point, start_point])
 
         return goal_joints_positions
 
     # Compute angles from cosine theorem and return them instead of positions
-    # TODO: refactor for N angles
-    def compute_goal_joints_angles(self, gp):
+    # IMPORTANT: function works only for RoboArm manipulator!
+    def compute_roboarm_joints_angles(self, gp):
         A = Point([0, 0, 0])
         B = Point([gp[0].x, gp[0].y, gp[0].z])
         C = Point([gp[1].x, gp[1].y, gp[1].z])
@@ -274,8 +274,6 @@ class Fabrik:
         BC = B.distance_to_point(C)
         CD = C.distance_to_point(D)
         DE = D.distance_to_point(E)
-
-        theta_1 = float(atan2(-gp[3].y, -gp[3].x))
 
         # first triangle
         ftr = [A, C]
@@ -292,46 +290,21 @@ class Fabrik:
         CE = C.distance_to_point(E)
         theta_4 = pi - acos((pow(CD,2) + pow(DE,2) - pow(CE,2)) / (2 * CD * DE))
         
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.scatter(round(self.init_joints_positions[0].x), round(self.init_joints_positions[0].y), round(self.init_joints_positions[0].z), color='blue')
-        ax.scatter([round(x.x) for x in base], [round(x.y) for x in base], [round(x.z) for x in base], color='green')
-        ax.plot3D([round(x.x) for x in base], [round(x.y) for x in base], [round(x.z) for x in base], color='green')
-        ax.scatter([round(x.x) for x in ftr], [round(x.y) for x in ftr], [round(x.z) for x in ftr], color='blue')
-        ax.plot3D([round(x.x) for x in ftr], [round(x.y) for x in ftr], [round(x.z) for x in ftr], color='blue')
-        ax.scatter([round(x.x) for x in sectr], [round(x.y) for x in sectr], [round(x.z) for x in sectr], color='yellow')
-        ax.plot3D([round(x.x) for x in sectr], [round(x.y) for x in sectr], [round(x.z) for x in sectr], color='yellow')
-        ax.scatter([round(x.x) for x in thrdtr], [round(x.y) for x in thrdtr], [round(x.z) for x in thrdtr], color='purple')
-        ax.plot3D([round(x.x) for x in thrdtr], [round(x.y) for x in thrdtr], [round(x.z) for x in thrdtr], color='purple')
-        ax.scatter([round(x.x) for x in self.init_joints_positions], [round(x.y) for x in self.init_joints_positions], [round(x.z) for x in self.init_joints_positions], color='green')
-        ax.plot3D([round(x.x) for x in self.init_joints_positions], [round(x.y) for x in self.init_joints_positions], [round(x.z) for x in self.init_joints_positions], color='green')
-        ax.scatter([round(x.x) for x in gp], [round(x.y) for x in gp], [round(x.z) for x in gp], color='pink')
-        ax.plot3D([round(x.x) for x in gp], [round(x.y) for x in gp], [round(x.z) for x in gp], color='pink')
-        ax.scatter(round(gp[0].x), round(gp[0].y), round(gp[0].z), color='orange')
-        plt.show()
+        self.plot_joints_and_points([base, ftr, sectr, thrdtr, self.init_joints_positions, gp], [self.init_joints_positions[0], gp])
         
+        theta_1 = float(atan2(-gp[3].y, -gp[3].x))
+
         return [theta_1, theta_2, theta_3, theta_4]
 
-    def compute_ik(self, goal_eff_pos, verbose = False):
+    def compute_roboarm_ik(self, goal_eff_pos, verbose = False):
         pos = self.compute_goal_joints_positions(goal_eff_pos, verbose)
-        return self.compute_goal_joints_angles(pos)
+        return self.compute_roboarm_joints_angles(pos)
 
 # test
 
 if __name__ == '__main__':
-    '''
-    point = Point([1,2,3])
-    joint = Joint('xyz', point)
-    print(joint)
-    joint2 = Joint('xzy', point)
-    print(joint2)
-    '''
-
     # Compute positions of all joints in robot init (base) position
-    dest_point = [-2, 1, 5.464]
+    dest_point = [3, 3, 4.464]
     theta_1 = float(atan2(dest_point[1], dest_point[0])) # compute theta_1
     print(theta_1)
     dh_matrix = [[theta_1, pi, -pi/3, -pi/3], [2, 0, 0, 0], [0, 2, 2, 2], [pi/2, 0, 0, 0]]
@@ -350,5 +323,5 @@ if __name__ == '__main__':
     out = fab.compute_goal_joints_positions(dest_point)
     PRINT_MSG('Goal joints positions:    ' + str(out))
 
-    ik_angles = fab.compute_ik(dest_point, True)
+    ik_angles = fab.compute_roboarm_ik(dest_point, True)
     PRINT_MSG('IK angles: ' + str(ik_angles))
